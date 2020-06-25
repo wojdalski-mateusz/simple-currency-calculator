8593a645a149aa83a8bb04c84b3e7403cea51639
Mateusz Wojdalski <wojdalski.m@gmail.com>
2020-06-25 22:51:23 +0200

    refactor JS, add funcions

--- js/script.js
+++ js/script.js
@@ -1,60 +1,72 @@
-let formElement = document.querySelector(".js-form")
-let amountToExchange = document.querySelector(".js-amountToExchange")
-let currencyToExchange = document.querySelector(".js-currencyToExchange")
-let exchangedCurrency = document.querySelector(".js-exchangedCurrency")
-let formResult = document.querySelector(".js-result")
+{
+    const calculateResult = (amount, whichCurrency, forWhichCurrency) => {
 
-let plnValue;
-let eurRate = 4.47;
-let usdRate = 3.99;
-let gbpRate = 4.96;
-let chfRate = 4.19;
+        let plnValue;
+        const eurRate = 4.47;
+        const usdRate = 3.99;
+        const gbpRate = 4.96;
+        const chfRate = 4.19;
 
-formElement.addEventListener("submit", (ev) => {
-    ev.preventDefault();
+        switch (whichCurrency) {
+            case "PLN":
+                plnValue = amount;
+                break;
+            case "EUR":
+                plnValue = amount * eurRate;
+                break;
+            case "USD":
+                plnValue = amount * usdRate;
+                break;
+            case "GBP":
+                plnValue = amount * gbpRate;
+                break;
+            case "CHF":
+                plnValue = amount * chfRate;
+                break;
+        }
 
-    let amount = +amountToExchange.value;
-    let whichCurrency = currencyToExchange.value;
-    let forWhichCurrency = exchangedCurrency.value;
-    let exchangeResult;
+        switch (forWhichCurrency) {
+            case "PLN":
+                return plnValue;
 
-    console.log(amount, whichCurrency, forWhichCurrency)
+            case "EUR":
+                return plnValue / eurRate;
 
-    switch (whichCurrency) {
-        case "PLN":
-            plnValue = amount;
-            break;
-        case "EUR":
-            plnValue = amount * eurRate;
-            break;
-        case "USD":
-            plnValue = amount * usdRate;
-            break;
-        case "GBP":
-            plnValue = amount * gbpRate;
-            break;
-        case "CHF":
-            plnValue = amount * chfRate;
-            break;
+            case "USD":
+                return plnValue / usdRate;
+
+            case "GBP":
+                return plnValue / gbpRate;
+
+            case "CHF":
+                return plnValue / chfRate;
+        }
+    };
+
+    const updateResultText = (amount, whichCurrency, exchangeResult, forWhichCurrency) => {
+        const formResult = document.querySelector(".js-result")
+        formResult.innerHTML = `<strong>${amount.toFixed(2)} ${whichCurrency} to ${exchangeResult.toFixed(2)} ${forWhichCurrency}</strong>`
     }
 
-    switch (forWhichCurrency) {
-        case "PLN":
-            exchangeResult = plnValue;
-            break;
-        case "EUR":
-            exchangeResult = plnValue / eurRate;
-            break;
-        case "USD":
-            exchangeResult = plnValue / usdRate;
-            break;
-        case "GBP":
-            exchangeResult = plnValue / gbpRate;
-            break;
-        case "CHF":
-            exchangeResult = plnValue / chfRate;
-            break;
-    }
+    const onFormSubmit = (ev) => {
+        ev.preventDefault();
 
-    formResult.innerHTML = `<strong>${amount.toFixed(2)} ${whichCurrency} to ${exchangeResult.toFixed(2)} ${forWhichCurrency}</strong>`
-});
+        const amountToExchange = document.querySelector(".js-amountToExchange")
+        const currencyToExchange = document.querySelector(".js-currencyToExchange")
+        const exchangedCurrency = document.querySelector(".js-exchangedCurrency")
+        const amount = +amountToExchange.value;
+        const whichCurrency = currencyToExchange.value;
+        const forWhichCurrency = exchangedCurrency.value;
+        let exchangeResult = calculateResult(amount, whichCurrency, forWhichCurrency);
+
+        updateResultText(amount, whichCurrency, exchangeResult, forWhichCurrency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
